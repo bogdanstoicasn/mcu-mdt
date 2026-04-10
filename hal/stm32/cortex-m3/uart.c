b1fc8e2595ce5fdb600c4822fe9504fb5870fd8e
bogdan <101634538+bogdanstoicasn@users.noreply.github.com>
2026-04-10 20:52:45 +0300

    cortex m3 work

--- hal/stm32/cortex-m3/uart.c
+++ hal/stm32/cortex-m3/uart.c
@@ -6,5 +6,82 @@ static ring_buffer_t tx_buffer = { .head = 0, .tail = 0 };
 
 void uart_init(uint32_t baudrate)
 {
-    
+    RCC->apb2enr |= RCC_APB2ENR_USART1EN;
+    RCC->ahbenr |= RCC_AHBENR_GPIOAEN;
+
+    /* PA9 config (pin 9 = CRH bits 4..7) (i think?) */
+
+    /* // PA10 config (pin 10 = CRH bits 8..11) (i think)*/
+
+    /* BRR = CLOCk / baud */
+    USART1->brr = (uint32_t)(F_CPU / baudrate);
+
+     /* Enable USART, TX, RX and RX interrupt */
+    USART1->cr1 |= USART_CR1_UE | USART_CR1_RE | USART_CR1_TE;
+
+    /* Enable NVIC for USART1 */
+    NVIC_ISER[USART1_IRQ / 32] = 1U << (USART1_IRQ % 32);
+}
+
+uint8_t uart_putc(uint8_t data)
+{
+    if (rb_is_full(&tx_buffer))
+    {
+        return 0;
+    }
+
+    rb_push(&tx_buffer, data);
+
+    USART1->cr1 |= USART_CR1_TXEIE;
+
+    return 1;
+}
+
+uint8_t uart_getc_nonblocking(uint8_t *data)
+{
+    return rb_pop(&rx_buffer, data);
+}
+
+uint8_t uart_ready(void)
+{
+    return !rb_is_full(&tx_buffer);
+}
+
+uint8_t uart_rx_overflow(void)
+{
+    if (rx_buffer.overflow_flag)
+    {
+        rx_buffer.overflow_flag = 0;
+        return 1;
+    }
+    return 0;
+}
+
+void USART1_IRQHandler(void)
+{
+    uint32_t sr = USART1->sr;
+
+    /* RX data received */
+    if (sr & USART_SR_RXNE)
+    {
+        uint8_t data = (uint8_t)USART1->dr;
+        if (!rb_push(&rx_buffer, data))
+            rx_buffer.overflow_flag = 1;
+    }
+
+    if (sr & USART_SR_TXE)
+    {
+        uint8_t data;
+        if (rb_pop(&tx_buffer, &data))
+            USART1->dr = data;
+        else
+            USART1->cr1 &= ~USART_CR1_TXEIE; /* disable TX interrupt */
+    }
+
+    /* Clear overrun error */
+    if (sr & USART_SR_ORE)
+    {
+        volatile uint32_t tmp = USART1->dr;
+        (void)tmp;
+    }
 }
--- hal/stm32/cortex-m3/uart.h
+++ hal/stm32/cortex-m3/uart.h
@@ -10,4 +10,76 @@ uint8_t uart_getc_nonblocking(uint8_t *data);
 uint8_t uart_ready(void);
 uint8_t uart_rx_overflow(void);
 
+/* USART1 */
+#define USART1_BASE 0x40013800UL
+
+typedef struct {
+    volatile uint32_t sr;   /* 0x00 */
+    volatile uint32_t dr;   /* 0x04 */
+    volatile uint32_t brr;  /* 0x08 */
+    volatile uint32_t cr1;  /* 0x0C */
+    volatile uint32_t cr2;  /* 0x10 */
+    volatile uint32_t cr3;  /* 0x14 */
+    volatile uint32_t gtpr; /* 0x18 */
+} usart_def_t;
+
+#define USART1 ((volatile usart_def_t *) USART1_BASE)
+
+#define USART_CR1_UE (1U << 13)
+#define USART_CR1_RE (1U << 2)
+#define USART_CR1_TE (1U << 3)
+#define USART_CR1_RXNEIE  (1U << 5)
+#define USART_CR1_TXEIE   (1U << 7)
+
+#define USART_SR_RXNE    (1U << 5)
+#define USART_SR_ORE    (1U << 3)
+#define USART_SR_TXE     (1U << 7)
+
+/* RCC */
+#define RCC_BASE 0x40021000UL
+
+typedef struct {
+    volatile uint32_t cr;       /* 0x00 */
+    volatile uint32_t cfgr;     /* 0x04 */
+    volatile uint32_t cir;      /* 0x08 */
+    volatile uint32_t apb2rstr; /* 0x0C */
+    volatile uint32_t apb1rstr; /* 0x10 */
+    volatile uint32_t ahbenr;   /* 0x14 */
+    volatile uint32_t apb2enr;  /* 0x18 */
+    volatile uint32_t apb1enr;  /* 0x1C */
+    volatile uint32_t bdcr;     /* 0x20 */
+    volatile uint32_t csr;      /* 0x24 */
+
+    /* Connectivity only devices */
+    volatile uint32_t ahbrstr;  /* 0x28 */
+    volatile uint32_t cfgr2;    /* 0x2C */
+} rcc_def_t;
+
+#define RCC ((volatile rcc_def_t *) RCC_BASE)
+
+#define RCC_APB2ENR_USART1EN  (1U << 14)
+#define RCC_AHBENR_GPIOAEN    (1U << 2)
+
+/* GPIOA */
+#define GPIOA_BASE 0x40010800UL
+
+typedef struct {
+    volatile uint32_t crl;  /* 0x00 */
+    volatile uint32_t crh;  /* 0x04 */
+    volatile uint32_t idr;  /* 0x08 */
+    volatile uint32_t odr;  /* 0x0C */
+    volatile uint32_t bsrr; /* 0x10 */
+    volatile uint32_t brr;  /* 0x14 */
+    volatile uint32_t lckr; /* 0x18 */
+
+} gpio_def_t;
+
+#define GPIOA ((volatile gpio_def_t *) GPIOA_BASE)
+
+
+/* NVIC */
+#define NVIC_ISER  ((volatile uint32_t *)0xE000E100)
+#define USART1_IRQ 37
+
+
 #endif
